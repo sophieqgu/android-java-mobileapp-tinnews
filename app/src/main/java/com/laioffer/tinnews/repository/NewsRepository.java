ed43a6b7f1b53586adc72894406d61cb746a57b6
Sophie Gu <sophieqgu@gmail.com>
2021-05-12 13:58:30 -0400

    create SaveViewModel

--- app/src/main/java/com/laioffer/tinnews/repository/NewsRepository.java
+++ app/src/main/java/com/laioffer/tinnews/repository/NewsRepository.java
@@ -14,6 +14,8 @@ import com.laioffer.tinnews.model.NewsResponse;
 import com.laioffer.tinnews.network.NewsApi;
 import com.laioffer.tinnews.network.RetrofitClient;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -71,6 +73,16 @@ public class NewsRepository {
             }
         }.execute();
         return isSuccessLiveData;
+    }
+
+    public LiveData<List<Article>> getAllSavedArticles() {
+        return database.dao().getAllArticles();
+    }
+
+    public void deleteSavedArticle(Article article) {
+        AsyncTask.execute(
+                () -> database.dao().deleteArticle()
+        );
     }
 
 
